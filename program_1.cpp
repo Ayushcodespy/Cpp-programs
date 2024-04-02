a7ae36c9ecb403b97989b6c3900bfc7ae37256a0
Ayushcodespy <ayush.jnv54@gmail.com>
2024-04-03 02:19:17 +0530

    Program of pointer to class

---
+++ program_1.cpp
@@ -0,0 +1,25 @@
+/* Create a class cube which have a data member side and a function which only print the value of side which has assigned in the in the pointer variable or pointer to class member */
+
+#include<iostream>
+using namespace std;
+
+class cube
+{
+    public:
+    int side;
+    void show()
+    {
+        cout<<"side="<<side;
+    }
+};
+
+int main()
+{
+    cube obj,*obj1;
+    obj1=&obj;
+    int cube :: *ptr = &cube:: side;
+    obj1->*ptr=20;
+    obj1->show();
+    return 0;
+}
+
