882410c4cf1a0bfaf70533e4ffb0f54e63d2b1f7
Ayushcodespy <ayush.jnv54@gmail.com>
2024-04-05 21:56:05 +0530

    program_6 is added

---
+++ program_6.cpp
@@ -0,0 +1,29 @@
+/* create a class simple have a simple function show() and create a pointer variable
+   inside the temp() simply print that variable after declaration */
+
+#include<iostream>
+using namespace std;
+
+class simple
+{
+    public:
+    void show()
+    {
+        cout<<"Inside the show function"<<endl;
+    }
+};
+
+void temp()
+{
+    int *pointer_variable = new int(10);
+    cout<<"Pointer variable : "<<pointer_variable<<endl;
+    delete  pointer_variable;
+}
+
+int main()
+{
+    simple obj;
+    obj.show();
+    temp();
+    return 0;
+}
